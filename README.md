6ec149a467efaccad3558ecd9b594d9b2bbf9253
Julián David <julianfullstackdev@gmail.com>
2022-01-23 18:21:08 -0500

    first commit, login mockup ready

---
+++ README.md
@@ -0,0 +1,7 @@
+# Art Pay NFT demo
+
+Art Pay demo for the Near Hackaton 😎
+
+## Demo preview
+
+Preview the demo here [NFT demo](https://artpay.com/):
---
+++ components/common/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <h1>Hey, I will be around later ;)</h1>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
