39f7ad6c592528fcd67ad6eff8da1b6465d1256b
Julián David <julianfullstackdev@gmail.com>
2022-01-28 21:11:40 -0500

    finish first step

--- components/artist/NewQuote/NewQuote.tsx
+++ components/artist/NewQuote/NewQuote.tsx
@@ -1,15 +1,4 @@
-import {
-  BsArrowRight,
-  BsCheck2,
-  BsFillChatFill,
-  BsFillJournalBookmarkFill,
-  BsFillPencilFill,
-} from 'react-icons/bs'
-import { BiPlusCircle } from 'react-icons/bi'
-import { MdShowChart } from 'react-icons/md'
-import { AiOutlineEllipsis } from 'react-icons/ai'
-import Image from 'next/image'
-import Link from 'next/link'
+import { HiOutlineSelector } from 'react-icons/hi'
 
 const NewQuote = () => {
   return (
@@ -34,14 +23,48 @@ const NewQuote = () => {
 
       <div>
         <h3 className="text-xl font-medium mb-5">What are you making?</h3>
+        <form action="">
+          <label className="flex flex-col mb-5">
+            <span className="font-medium mb-2">Project Title</span>
+            <input
+              type="text"
+              className="shadow-sm shadow-gray-300 border-gray-100 px-4 py-2 rounded-sm"
+            />
+          </label>
 
-        <label className="flex flex-col">
-          <span className="font-medium mb-2">Project Title</span>
-          <input
-            type="text"
-            className="shadow-sm shadow-gray-300 border-gray-100 px-4 py-2 rounded-sm"
-          />
-        </label>
+          <label className="flex flex-col mb-5">
+            <span className="font-medium mb-2">Type</span>
+            <div className="relative w-full">
+              <select className="shadow-sm shadow-gray-300 border-gray-100 px-4 py-2 rounded-sm bg-white w-full">
+                <option value="">Custom Artwork</option>
+                <option value="">Another option</option>
+                <option value="">Yeah, another option</option>
+              </select>
+              <HiOutlineSelector className="absolute right-2 bottom-2 text-2xl" />
+            </div>
+          </label>
+
+          <label className="flex flex-col mb-5">
+            <span className="font-medium mb-2">Description</span>
+            <textarea
+              rows={3}
+              className="shadow-sm shadow-gray-300 border-gray-100 px-4 py-2 rounded-sm"
+            ></textarea>
+          </label>
+
+          <div className="flex justify-end gap-4">
+            <button
+              type="button"
+              className="px-3 border border-gray-400 rounded-sm py-1 font-medium"
+              onClick={() => window.history.back()}
+            >
+              Cancel
+            </button>
+            <button className="px-3 rounded-sm py-1 bg-primary text-white font-medium">
+              Next Step
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   )
