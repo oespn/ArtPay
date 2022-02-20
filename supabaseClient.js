3e01b33dccbb6ce12ee1a6256438896a57638920
quocanh <quocanh16linh@gmail.com>
2022-02-20 13:03:07 +0700

    Add config for Supabase

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-
-const supabaseUrl = '**OBFUSCATED'
-const supabaseAnonKey = '**OBFUSCATED'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
